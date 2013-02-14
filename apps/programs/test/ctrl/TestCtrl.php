16cd2a85a3212b776b8170e4062abfa6429f4b53
ddadick <ddadick@bigmir.net>
2013-02-14 02:11:29 +0200

    add

---
+++ apps/programs/test/ctrl/TestCtrl.php
@@ -0,0 +1,14 @@
+<?php
+class Test_TestCtrl extends Apps_Programs_Kernel
+{
+	public function TestMethod(){
+		echo '1';
+		//echo 'asdasd';
+		$this->rendered->ad='TEST';
+		$this->rendered->array=array('crrr','sadasd','wrwerwer');
+		$this->rendered->array2=array('vvv','xxx','cccc');
+		$this->rendered->array3=array('1','2','3');
+		echo 'adadad';
+		//Dadiweb_Aides_Debug::show($this->rendered->ad);
+	}
+}
